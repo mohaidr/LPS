dd5cf2d882a50160db6a367eb5a03560a8015a22
Mohamad <mahdi.com.haidar@gmail.com>
2024-08-30 14:32:45 +0300

    introduce the new LPS Dashboard

---
+++ LPS.Infrastructure/Common/Interfaces/ILPSConnectionsMetricMonitor.cs
@@ -0,0 +1,16 @@
+﻿using LPS.Domain;
+using LPS.Domain.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPS.Infrastructure.Common.Interfaces
+{
+    public interface ILPSConnectionsMetricMonitor : ILPSMetricMonitor
+    {
+        public bool IncreaseConnectionsCount();
+        public bool DecreseConnectionsCount(bool isSuccess);
+    }
+}
