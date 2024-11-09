ba5ce2fa31f6328473f30a085770c868f1fcfe0b
Mohamad <mahdi.com.haidar@gmail.com>
2024-11-09 04:03:57 +0300

    added removed files back with correct case sensitivity

---
+++ LPS/UI.Core/LPSCommandLine/Commands/IterationCliCommand.cs
@@ -0,0 +1,121 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using LPS.Domain;
+using LPS.Domain.Common.Interfaces;
+using LPS.DTOs;
+using LPS.Infrastructure.Common;
+using LPS.UI.Common;
+using LPS.UI.Common.Extensions;
+using LPS.UI.Core.LPSCommandLine.Bindings;
+using LPS.UI.Core.LPSValidators;
+using LPS.UI.Core.Services;
+using Spectre.Console;
+using System;
+using System.CommandLine;
+
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace LPS.UI.Core.LPSCommandLine.Commands
+{
+    internal class IterationCliCommand : ICliCommand
+    {
+        private readonly Command _rootCliCommand;
+        private Command _iterationCommand;
+        public Command Command => _iterationCommand;
+        private RefCliCommand _refCliCommand;
+        ILogger _logger;
+        IRuntimeOperationIdProvider _runtimeOperationIdProvider;
+        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        internal IterationCliCommand(Command rootCliCommand, ILogger logger, IRuntimeOperationIdProvider runtimeOperationIdProvider)
+        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _rootCliCommand = rootCliCommand;
+            _logger = logger;
+            _runtimeOperationIdProvider = runtimeOperationIdProvider;
+            Setup();
+        }
+        private void Setup()
+        {
+            _iterationCommand = new Command("iteration", "Add an http iteration")
+            {
+                CommandLineOptions.LPSIterationCommandOptions.ConfigFileArgument // Add ConfigFileArgument here
+            };
+            CommandLineOptions.AddOptionsToCommand(_iterationCommand, typeof(CommandLineOptions.LPSIterationCommandOptions));
+            _refCliCommand = new RefCliCommand(_iterationCommand, _logger, _runtimeOperationIdProvider);
+            _rootCliCommand.AddCommand(_iterationCommand);
+        }
+        public void SetHandler(CancellationToken cancellationToken)
+        {
+            _iterationCommand.SetHandler((configFile, roundName, iteration, isGlobal) =>
+            {
+                var iterationValidator = new IterationValidator(iteration);
+                ValidationResult results = iterationValidator.Validate();
+
+                if (results.IsValid)
+                {
+                    try
+                    {
+                        var planDto = ConfigurationService.FetchConfiguration<PlanDto>(configFile);
+                        bool isRoundNameEmpty = string.IsNullOrEmpty(roundName);
+                        // Determine where to add the iteration based on the global and roundName options
+                        if (isGlobal || isRoundNameEmpty)
+                        {
+                            var existingGlobalIteration = planDto.Iterations.FirstOrDefault(i => i.Name == iteration.Name);
+                            if (existingGlobalIteration != null)
+                            {
+                                planDto.Iterations.Remove(existingGlobalIteration);
+                            }
+                            // Add iteration to global iterations
+                            planDto.Iterations.Add(iteration);
+                        }
+
+                        if (!isRoundNameEmpty)
+                        {
+                            var round = planDto.Rounds.FirstOrDefault(r => r.Name == roundName);
+
+                            if (round != null)
+                            {
+                                var existingRoundIteration = round.Iterations.FirstOrDefault(i => i.Name == iteration.Name);
+                                if (existingRoundIteration != null)
+                                {
+                                    round.Iterations.Remove(existingRoundIteration);
+                                }
+                                if (isGlobal)
+                                {
+                                    // Add as a global iteration but also create a setup command in the round with the same name
+                                    round.Iterations.Add(new HttpIterationDto { Name = iteration.Name });
+                                }
+                                else
+                                {
+                                    // Add iteration as a local iteration within the round
+                                    round.Iterations.Add(iteration);
+                                }
+                            }
+                            else
+                            {
+                                throw new ArgumentException($"Invalid Round Name {roundName}");
+                            }
+                        }
+
+                        ConfigurationService.SaveConfiguration(configFile, planDto);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    results.PrintValidationErrors();
+                }
+
+            },
+            CommandLineOptions.LPSIterationCommandOptions.ConfigFileArgument,
+            CommandLineOptions.LPSIterationCommandOptions.RoundNameOption,
+            new IterationCommandBinder(),
+            CommandLineOptions.LPSIterationCommandOptions.GlobalOption);
+
+            _refCliCommand.SetHandler(cancellationToken);
+        }
+    }
+}
---
+++ LPS/UI.Core/LPSCommandLine/Commands/RoundCliCommand.cs
@@ -0,0 +1,74 @@
+﻿using LPS.Domain;
+using LPS.DTOs;
+using LPS.Infrastructure.Common;
+using LPS.UI.Common;
+using LPS.UI.Common.Extensions;
+using LPS.UI.Core.LPSCommandLine.Bindings;
+using LPS.UI.Core.LPSValidators;
+using LPS.UI.Core.Services;
+using System.CommandLine;
+using System.Xml.Linq;
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace LPS.UI.Core.LPSCommandLine.Commands
+{
+    internal class RoundCliCommand : ICliCommand
+    {
+        private Command _rootCliCommand;
+        private string[] _args;
+        private Command _roundCommand;
+        public Command Command => _roundCommand;
+        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        internal RoundCliCommand(Command rootLpsCliCommand, string[] args)
+        {
+            _rootCliCommand = rootLpsCliCommand;
+            _args = args;
+            Setup();
+        }
+
+        private void Setup()
+        {
+            _roundCommand = new Command("round", "Create a new test")  
+            {
+                CommandLineOptions.LPSRoundCommandOptions.ConfigFileArgument // Add ConfigFileArgument here
+            };
+            CommandLineOptions.AddOptionsToCommand(_roundCommand, typeof(CommandLineOptions.LPSRoundCommandOptions));
+            _rootCliCommand.AddCommand(_roundCommand);
+        }
+
+        public void SetHandler(CancellationToken cancellationToken)
+        {
+            _roundCommand.SetHandler((configFile, round) =>
+            {
+                ValidationResult results;
+                var roundValidator = new RoundValidator(round);
+                results = roundValidator.Validate();
+                if (!results.IsValid)
+                {
+                    results.PrintValidationErrors();
+                }
+                else
+                {
+                    PlanDto planDto = ConfigurationService.FetchConfiguration<PlanDto>(configFile);
+                    var selectedRound = planDto?.Rounds.FirstOrDefault(r => r.Name == round.Name);
+                    if (selectedRound != null)
+                    {
+                        selectedRound.Name = round.Name;
+                        selectedRound.StartupDelay = round.StartupDelay;
+                        selectedRound.NumberOfClients = round.NumberOfClients;
+                        selectedRound.ArrivalDelay = round.ArrivalDelay;
+                        selectedRound.DelayClientCreationUntilIsNeeded = round.DelayClientCreationUntilIsNeeded;
+                        selectedRound.RunInParallel = round.RunInParallel;
+                    }
+                    else
+                    {
+                        planDto?.Rounds.Add(round);
+                    }
+                    ConfigurationService.SaveConfiguration(configFile, planDto);
+                }
+            },
+            CommandLineOptions.LPSRoundCommandOptions.ConfigFileArgument,
+            new RoundCommandBinder());
+        }
+    }
+}
