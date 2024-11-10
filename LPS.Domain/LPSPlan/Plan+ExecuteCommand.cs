841699d0105d52d07402d79e78e3963c734cac50
Mohamad <mahdi.com.haidar@gmail.com>
2024-11-11 01:54:05 +0300

    fix a UI related bug.

--- LPS.Domain/LPSPlan/Plan+ExecuteCommand.cs
+++ LPS.Domain/LPSPlan/Plan+ExecuteCommand.cs
@@ -99,7 +99,6 @@ namespace LPS.Domain
                         _cts);
                     await roundExecCommand.ExecuteAsync(round);
                 }
-
             }
         }
 
@@ -110,7 +109,9 @@ namespace LPS.Domain
                 foreach (var iteration in round.GetReadOnlyIterations())
                 {
                     if (iteration.Type == IterationType.Http)
+                    {
                         _lpsMetricsDataMonitor.TryRegister(round.Name, (HttpIteration)iteration);
+                    }
                 }
             }
         }
