5430f40673e07589c2b7a078bef8d967630a7104
Mohamad <mahdi.com.haidar@gmail.com>
2024-12-14 02:25:27 +0300

    start supporting different payload types.

---
+++ LPS.Domain/LPSSession/LPSHttpRequest/Payload.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LPS.Domain.LPSSession
+{
+    public class Payload
+    {
+        public enum PayloadType
+        {
+            Raw,
+            Multipart,
+            Binary
+        }
+
+        public PayloadType Type { get; private set; }
+        public string RawValue { get; private set; }
+        public Dictionary<string, object> MultipartData { get; private set; }
+        public byte[] BinaryData { get; private set; }
+
+        private Payload(PayloadType type)
+        {
+            Type = type;
+        }
+
+        public static Payload CreateRaw(string rawValue)
+        {
+            return new Payload(PayloadType.Raw)
+            {
+                RawValue = rawValue
+            };
+        }
+
+        public static Payload CreateMultipart(Dictionary<string, object> multipartData)
+        {
+            return new Payload(PayloadType.Multipart)
+            {
+                MultipartData = multipartData
+            };
+        }
+
+        public static Payload CreateBinary(byte[] binaryData)
+        {
+            return new Payload(PayloadType.Binary)
+            {
+                BinaryData = binaryData
+            };
+        }
+    }
+}
